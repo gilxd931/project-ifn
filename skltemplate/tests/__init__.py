818878c513dfb46bba4daf427b1c3b05418c3474
gilr <gilr@capitolis.com>
2019-01-02 20:59:52 +0200

    try

--- skltemplate/tests/__init__.py
+++ skltemplate/tests/__init__.py
@@ -1,8 +1,15 @@
 
 from skltemplate import IfnClassifier
 from sklearn.datasets import load_iris
+import pandas as pd
 
+dt= pd.read_csv("test_dataset_1.csv", encoding='latin1')
+
+print (dt)
 iris = load_iris()
 
-clf = IfnClassifier(iris.data, iris.target)
-
+x = [[1,1,0], [0,1,1], [1,0,1], [0,1,0] ]
+y= [1, 0 , 0, 1]
+clf = IfnClassifier()
+clf.fit(x, y)
+x=3

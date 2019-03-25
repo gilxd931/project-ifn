d9ad568086d0984066eff4f7faa3e4a5406598be
gilr <gilr@capitolis.com>
2019-03-25 17:47:37 +0200

    add beginning of unit tests

--- skltemplate/tests/test_template.py
+++ skltemplate/tests/test_template.py
@@ -1,64 +1,41 @@
 import pytest
 import numpy as np
 
-from sklearn.datasets import load_iris
+from sklearn.datasets import load_iris, load_breast_cancer
 from sklearn.utils.testing import assert_array_equal
 from sklearn.utils.testing import assert_allclose
-
-from skltemplate import TemplateEstimator
-from skltemplate import TemplateTransformer
-from skltemplate import TemplateClassifier
-
+from skltemplate import IfnClassifier
 
 @pytest.fixture
 def data():
     return load_iris(return_X_y=True)
 
 def test_template_estimator(data):
-    est = TemplateEstimator()
-    assert est.demo_param == 'demo_param'
+    clf = IfnClassifier()
 
-    est.fit(*data)
-    assert hasattr(est, 'is_fitted_')
+    clf.fit(*data)
+    assert hasattr(clf, 'is_fitted_') # hasattr means- as attribute.
 
     X = data[0]
-    y_pred = est.predict(X)
+    y_pred = clf.predict(X)
     assert_array_equal(y_pred, np.ones(X.shape[0], dtype=np.int64))
 
 
-def test_template_transformer_error(data):
-    X, y = data
-    trans = TemplateTransformer()
-    trans.fit(X)
-    with pytest.raises(ValueError, match="Shape of input is different"):
-        X_diff_size = np.ones((10, X.shape[1] + 1))
-        trans.transform(X_diff_size)
+def test_class_layer(data):
+    clf = IfnClassifier()
 
+    # test number of classes in iris
+    clf.fit(*data)
+    assert 3 == len(clf.network.target_layer)
 
-def test_template_transformer(data):
-    X, y = data
-    trans = TemplateTransformer()
-    assert trans.demo_param == 'demo'
+    # test number of classes in digits
+    # clf.fit(digits.data, digits.target)
+    # self.assertEqual(13, len(clf.network.target_layer))
 
-    trans.fit(X)
-    assert trans.n_features_ == X.shape[1]
+    # test number of classes in breast_cancer
+    # clf.fit(breast_cancer.data, breast_cancer.target)
+    # self.assertEqual(2, len(clf.network.target_layer))
 
-    X_trans = trans.transform(X)
-    assert_allclose(X_trans, np.sqrt(X))
+    # test number of classes in wine
+    # clf.fit(wine.data, wine.target)
 
-    X_trans = trans.fit_transform(X)
-    assert_allclose(X_trans, np.sqrt(X))
-
-
-def test_template_classifier(data):
-    X, y = data
-    clf = TemplateClassifier()
-    assert clf.demo_param == 'demo'
-
-    clf.fit(X, y)
-    assert hasattr(clf, 'classes_')
-    assert hasattr(clf, 'X_')
-    assert hasattr(clf, 'y_')
-
-    y_pred = clf.predict(X)
-    assert y_pred.shape == (X.shape[0],)

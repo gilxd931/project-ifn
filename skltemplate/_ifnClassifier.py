f27e11a7dea0d80ff2c6c3b0fbd450b222164ddb
tzuriel1988 <tzuri1988@gmail.com>
2019-03-26 16:55:04 +0200

    create mi

--- skltemplate/_ifnClassifier.py
+++ skltemplate/_ifnClassifier.py
@@ -9,10 +9,22 @@ from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 from sklearn import metrics
 from sklearn.utils.multiclass import unique_labels
 from sklearn.metrics import euclidean_distances
-from pyitlib import discrete_random_variable as drv
+
 
 
 from scipy import stats
+
+
+def drop_records(X, index_atr, y, index_node):
+    ans_x = []
+    ans_y = []
+    for i in range(len(y)):
+        if X[i][index_atr] == index_node:
+            ans_x.append(X[i])
+            ans_y.append(y[i])
+    return np.array(ans_x), np.array(ans_y)
+
+
 
 class IfnClassifier():
     attributes_array=[]
@@ -27,6 +39,7 @@ class IfnClassifier():
     demo_param : str, default='demo_param'
         A parameter used for demonstation of how to pass and store paramters.
     """
+
     def __init__(self, alpha = 0.99):
         self.alpha = alpha
         self.network = IfnNetwork()
@@ -79,7 +92,8 @@ class IfnClassifier():
         self.network.root_node.first_layer = first_layer
         nodes_list = []
         for i in unique_values_per_attribute[chosen_attribute]:
-            nodes_list.append(AttributeNode(i, chosen_attribute))
+            x_y_tuple = drop_records(X, chosen_attribute, y, i)
+            nodes_list.append(AttributeNode(i, chosen_attribute, x_y_tuple[0], x_y_tuple[1]))
         first_layer.set_nodes(nodes_list)
         current_layer = first_layer
 
@@ -89,26 +103,44 @@ class IfnClassifier():
         while len(updated_attributes_array) > 0:
             # get the attribute that holds the maximal mutual information
             nodes_info_per_attribute = {}
-            for attribute in updated_attributes_array:
-                attribute_data = []
-                for record in X:
-                    attribute_data.append(record[attribute])
-                unique_values_per_attribute[attribute] = np.unique(attribute_data)
-                total_attribute_mi = 0
-                nodes_info_list = []
-                for node in current_layer.nodes:
-                    node_mi = random.uniform(0, 1)#calc_node_MI()
-                    total_attribute_mi += node_mi
+            for node in current_layer.nodes:
+                total_node_mi=0
+                for attribute in updated_attributes_array:
+                    if nodes_info_per_attribute[attribute] is None:
+                        nodes_info_per_attribute[attribute] = []
+                    attribute_data = []
+                    for record in node.partial_x:
+                        attribute_data.append(record[attribute])
+                    nodes_info_list = []
+                    total_attribute_mi = 0
+                    unique_values_per_attribute[attribute] = np.unique(attribute_data)
+                    node_mi = metrics.mutual_info_score(attribute_data, node.partial_y)
+                    total_node_mi+=node_mi
                     statistic = 2 * np.log(2) * total_records * node_mi
                     critical = stats.chi2.ppf(self.alpha, 1)
+                    node_info_tuple = (node.index, node_mi, True)
                     if critical < statistic:
-                        nodes_info_list.append((node.index, node_mi, True)) # True means split
+                        node_info_tuple = (node.index, node_mi, True)
                     else:
-                        nodes_info_list.append((node.index, node_mi, False))
+                        node_info_tuple = (node.index, 0, False)
 
-                nodes_info_per_attribute[attribute] = nodes_info_list
-                nodes_info_list = []
-                attributes_mi[attribute] = total_attribute_mi
+                    nodes_info_per_attribute[attribute] = nodes_info_per_attribute[attribute].append(nodes_info_list)
+
+            max_node_mi=0
+            index_atr=0
+            for key, value in nodes_info_per_attribute:
+                node_mi=0
+                for node_tuple in value:
+                    node_mi+=node_tuple[1]
+                    if max_node_mi < node_mi:
+                        max_node_mi = max(max_node_mi, node_mi)
+                        index_atr = key
+
+
+
+
+
+
             chosen_attribute = max(attributes_mi, key=attributes_mi.get)
 
             # set terminal nodes
@@ -150,7 +182,10 @@ class IfnClassifier():
         return self
 
 
-    def calIMPerNode(self,X,y):
+
+
+
+    def calIMPerNode(self, X, y):
         attributes_mi = {}
 
         for attribute in self.updated_attributes_array:

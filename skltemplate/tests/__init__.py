94f971d63ef0e4c3df1e7fe6b028132ee02b275a
gilxd931 <gilxd931@gmail.com>
2019-07-25 10:01:30 +0300

    Update __init__.py

--- skltemplate/tests/__init__.py
+++ skltemplate/tests/__init__.py
@@ -5,21 +5,18 @@ clf = IfnClassifier(0.999)
 x = []
 
 
-# CREDIT
-
-data = _csvConveter.CsvConverter.convert("glass.csv")
+data = _csvConveter.CsvConverter.convert("Credit_full.csv")
 
 clf.fit(data[0], data[1], data[2])
 clf.add_training_set_error_rate(data[0], data[1])
-
 clf.network.create_network_structure_file()
 
+
+z = _csvConveter.CsvConverter.convert_predict("pred_credit.csv")
 # -------------- predict will return the classes and write it to file --------------
 
-# print(clf.predict([[0, 2, 5, 4, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1],
-#                          [1, 41, 5, 3, 0, 1, 2, 0, 0, 14, 1, 0, 500, 159]]))
+# print(clf.predict(z)
 
 # -------------- predict_proba will return the probability for every class and write it to file --------------
-print(clf.predict_proba([[0, 2, 5, 4, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1],
-                         [1, 41, 5, 3, 0, 1, 2, 0, 0, 14, 1, 0, 500, 159]]))
+print(clf.predict_proba(z))
 
